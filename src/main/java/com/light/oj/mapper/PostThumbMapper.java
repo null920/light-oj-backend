cfc95ed9bed9f92ffc21e80d8da6093a5318a51f
null920 <3590543392@qq.com>
2024-06-13 22:24:36 +0800

    first commit

---
+++ src/main/java/com/light/oj/mapper/PostThumbMapper.java
@@ -0,0 +1,15 @@
+package com.light.oj.mapper;
+
+import com.light.oj.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/light/oj/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.light.oj.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.light.oj.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
